80fa80012e2230d29ccf06a36023aee9d9e56bfb
GuillermoNforgeflow <guillermo.navas@forgeflow>
2025-09-29 13:26:29 +0200

    activacio modul

---
+++ __manifest__.py
@@ -0,0 +1,15 @@
+{
+    'name': "tienda_peluches",
+    'summary': "Compra peluches",
+    'description': "Peluches a buen precio.",
+    'author': "PeluchesSL",
+    'website': "http://www.peluches.com",
+    'category': 'Uncategorized',
+    'depends': ['base'],
+    "data": [
+        #"security/hostel_security.xml",
+        #"security/ir.model.access.csv",
+        #"views/hostel.xml",
+        #"data/data.xml"
+    ],
+}
